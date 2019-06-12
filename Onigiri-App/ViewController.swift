d2e07e335f1efd2b764305c023aa510d06e7f5bb
Vale <valeria_luce@hotmail.com>
2019-06-12 10:11:54 +0200

    Initial Commit

---
+++ Onigiri-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Onigiri-App
+//
+//  Created by Vale-chan on 12.06.19.
+//  Copyright © 2019 Vale-chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

c920880c7723feb2dad004641f5f79fefcccb1da
Vale <valeria_luce@hotmail.com>
2019-06-13 20:34:10 +0200

    added part 3 of chapter 4, guided project

---
+++ Onigiri-App/Reflection.swift
@@ -0,0 +1,16 @@
+//
+//  Reflection.swift
+//  Onigiri-App
+//
+//  Created by Vale-chan on 13.06.19.
+//  Copyright © 2019 Vale-chan. All rights reserved.
+//
+
+import Foundation
+
+struct Reflection {
+    var answer1: String
+    var answer2: String
+    var answer3: String
+    var answer4: String
+}
--- Onigiri-App/ReflectionListTableViewController.swift
+++ Onigiri-App/ReflectionListTableViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ReflectionListTableViewController: UITableViewController {
     
+    @IBOutlet weak var textLabel: UILabel!
+    
+    
+    var reflections = [Reflection]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,8 +28,7 @@ class ReflectionListTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return reflections.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -33,30 +37,25 @@ class ReflectionListTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reflectionTableViewCell", for: indexPath)
+        let reflection = reflections[indexPath.row]
+        cell.textLabel?.text = reflection.answer1
 
         return cell
     }
 
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            reflections.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        } //else if editingStyle == .insert {}    
     }
-    */
 
     /*
     // Override to support rearranging the table view.

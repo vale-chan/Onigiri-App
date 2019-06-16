bb6c562b32f70846a24842ccc2d2b4a2dae007c7
Vale <valeria_luce@hotmail.com>
2019-06-16 07:46:29 +0200

    implemented TableVC in TabVC

---
+++ Onigiri-App/Reflection.swift
@@ -0,0 +1,18 @@
+//
+//  Reflection.swift
+//  Onigiri-App
+//
+//  Created by Vale-chan on 16.06.19.
+//  Copyright © 2019 Vale-chan. All rights reserved.
+//
+
+import UIKit
+
+struct Reflection {
+    
+    var date: Date
+    var answer1: String
+    var answer2: String?
+    var answer3: String?
+    var answer4: String?
+}
